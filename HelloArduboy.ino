570786fdb539965dbb51eedbcbd9522314dbf434
ishotjr <github@ishotjr.com>
2016-10-24 23:06:14 -0400

    :video_game: :dash: Add button input, variable velocity, sound toggle, and reset

--- HelloArduboy.ino
+++ HelloArduboy.ino
@@ -13,10 +13,15 @@
 
 #include <Arduboy.h>
 
+#define MAX_VELOCITY 5
+
 Arduboy arduboy;
 
-static int8_t x = 0;
-static uint8_t y = 0;
+static int8_t x_position = 0;
+static int8_t y_position = 0;
+static int8_t x_velocity = 1;
+static int8_t y_velocity = 1;
+static boolean sound_enabled = 1;
 
 void setup() {
   arduboy.begin();
@@ -35,27 +40,77 @@ void loop() {
   // intentionally adding to buffer :D
   //arduboy.clear();
 
-  // move text diagonally across the screen (looping at limits)
-  arduboy.setCursor(x++, y++);
+  // input
+  // TODO: some kind of de-bounce or smoothing?!
+  if (arduboy.pressed(UP_BUTTON)) {
+    y_velocity++;
+    if (y_velocity > MAX_VELOCITY) {
+      y_velocity = MAX_VELOCITY;
+    }
+  } else if (arduboy.pressed(DOWN_BUTTON)) {
+    y_velocity--;
+    if (y_velocity < -1 * MAX_VELOCITY) {
+      y_velocity = -1 * MAX_VELOCITY;
+    }
+  } else if (arduboy.pressed(RIGHT_BUTTON)) {
+    x_velocity++;
+    if (x_velocity > MAX_VELOCITY) {
+      x_velocity = MAX_VELOCITY;
+    }
+  } else if (arduboy.pressed(LEFT_BUTTON)) {
+    x_velocity--;
+    if (x_velocity < -1 * MAX_VELOCITY) {
+      x_velocity = -1 * MAX_VELOCITY;
+    }
+  } else if (arduboy.pressed(A_BUTTON)) {
+    // reset
+    x_position = 0;
+    y_position = 0;
+    x_velocity = 1;
+    y_velocity = 1;
+    sound_enabled = 1;
+    arduboy.clear();
+  } else if (arduboy.pressed(B_BUTTON)) {
+    // toggle sound
+    sound_enabled = !sound_enabled;
+  }
 
-  //arduboy.fillCircle(x - 8, y, 4, WHITE);
+  // move text diagonally across the screen (looping at limits)
+  x_position = x_position + x_velocity;
+  y_position = y_position + y_velocity;
+  arduboy.setCursor(x_position, y_position);
+
+  //arduboy.fillCircle(x_position - 8, y_position, 4, WHITE);
 
   arduboy.print("LOOK AROUND YOU");
 
   arduboy.display();
 
-  arduboy.setRGBled((arduboy.width() - x) * (255 / arduboy.width()), 
-    (arduboy.height() - y) * (255 / arduboy.height()), 0);
-    //(x + y) * (255 / (arduboy.width() + arduboy.height())));
+  // express velocities via as a percentage of red/green intensity from 0 for -MAX_VELOCITY) to 255 for MAX_VELOCITY
+  // blue indicates change in position
+  // TODO: LED colors not quite as expected - reduce/offset intensity?
+  arduboy.setRGBled(((MAX_VELOCITY + x_velocity) * 255) / (MAX_VELOCITY * 2), 
+    ((MAX_VELOCITY + y_velocity) * 255) / (MAX_VELOCITY * 2), 
+    ((x_position + y_position) * 255) / (arduboy.width() + arduboy.height()));
 
-  arduboy.tunes.tone((arduboy.width() - x) + (arduboy.height() - y) * 10, 1000 / 60);
-    
-  if (y > arduboy.height()) {
-    y = 0;
-    //x = arduboy.height();
+  if (sound_enabled) {
+    arduboy.tunes.tone((arduboy.width() - x_position) + (arduboy.height() - y_position) * 10, 1000 / 60);
   }
-  if (x > arduboy.width()) {
-    x = 0 - arduboy.width();
+    
+  if (y_position > arduboy.height()) {
+    y_position = 0;
+    //x_position = arduboy.height();
+  } else if (y_position < (-1 * arduboy.height())) {
+    // loop back the other way too!
+    // TODO: w/b better if comparison was to text height
+    y_position = arduboy.height();
+  }
+  if (x_position > arduboy.width()) {
+    x_position = 0 - arduboy.width();
+  } else if (x_position < (-1 * arduboy.width())) {
+    // loop back the other way too!
+    // TODO: w/b better if comparison was to text width
+    x_position = arduboy.width();
   }
 
 }
