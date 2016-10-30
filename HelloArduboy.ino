dd5b7512cf9e8a9410b03668a615c9504e9bc90a
ishotjr <github@ishotjr.com>
2016-10-30 00:06:23 -0400

    :rocket: :green_apple: Add jump, gravity

--- HelloArduboy.ino
+++ HelloArduboy.ino
@@ -17,6 +17,7 @@
 #define DEBUG true
 
 #define MAX_VELOCITY 2
+#define MAX_FREEFALL 4
 
 Arduboy arduboy;
 
@@ -57,29 +58,33 @@ void loop() {
     if (y_velocity < -1 * MAX_VELOCITY) {
       y_velocity = -1 * MAX_VELOCITY;
     }
-    no_movement = 0;
-  } else if (arduboy.pressed(DOWN_BUTTON)) {
+    //no_movement = 0;
+  }
+  if (arduboy.pressed(DOWN_BUTTON)) {
     y_position++;
     y_velocity++;
     if (y_velocity > MAX_VELOCITY) {
       y_velocity = MAX_VELOCITY;
     }
-    no_movement = 0;
-  } else if (arduboy.pressed(RIGHT_BUTTON)) {
+    //no_movement = 0;
+  }
+  if (arduboy.pressed(RIGHT_BUTTON)) {
     x_position++;
     x_velocity++;
     if (x_velocity > MAX_VELOCITY) {
       x_velocity = MAX_VELOCITY;
     }
     no_movement = 0;
-  } else if (arduboy.pressed(LEFT_BUTTON)) {
+  }
+  if (arduboy.pressed(LEFT_BUTTON)) {
     x_position--;
     x_velocity--;
     if (x_velocity < -1 * MAX_VELOCITY) {
       x_velocity = -1 * MAX_VELOCITY;
     }
     no_movement = 0;
-  } else if (arduboy.pressed(A_BUTTON)) {
+  }
+  if (arduboy.pressed(A_BUTTON)) {
     // reset
     x_position = 0;
     y_position = 0;
@@ -87,9 +92,15 @@ void loop() {
     y_velocity = 0;
     sound_enabled = 1;
     arduboy.clear();
-  } else if (arduboy.pressed(B_BUTTON)) {
-    // toggle sound
-    sound_enabled = !sound_enabled;
+  }
+  if (arduboy.pressed(B_BUTTON)) {
+    // jump
+
+    // prevent flying/freefall jump
+    // TODO: add check for ground below!
+    if (y_velocity == 0) {
+      y_velocity = -5;
+    }
   }
 
 
@@ -103,6 +114,7 @@ void loop() {
     y_position = arduboy.height();
   }
   // note comparison to (x_velocity + 1) to prevent/correct overflow (range of int8_t is only 127)
+  // TODO: there's still an issue in certain cases
   if (x_position > (arduboy.width() - (x_velocity + 1))) {
     x_position = -1 * SPRITE_SIZE;
   } else if (x_position < (-1 * SPRITE_SIZE)) {
@@ -111,7 +123,7 @@ void loop() {
   }
 
 
-  // ease to a stop if no input for 3 frames
+  // ease to a stop horizontally if no input for 3 frames
   if (no_movement > 2) {
     // TODO: refactor?
     if (x_velocity > 0) {
@@ -119,13 +131,19 @@ void loop() {
     } else if (x_velocity < 0) {
       x_velocity++;
     }
-
-    if (y_velocity > 0) {
-      y_velocity--;
-    } else if (y_velocity < 0) {
-      y_velocity++;
-    }
   }
+
+  // gravity
+  // *prevent fall-through due to velocity
+  if (y_position < (arduboy.height() - SPRITE_SIZE - y_velocity)) {
+    y_velocity++;
+    if (y_velocity > MAX_FREEFALL) {
+      y_velocity = MAX_FREEFALL;
+    }
+  } else {
+    y_velocity = 0;
+  }
+
   x_position = x_position + x_velocity;
   y_position = y_position + y_velocity;
   arduboy.setCursor(x_position, y_position);
@@ -148,7 +166,9 @@ void loop() {
 #if DEBUG
   // font is 6x8
   arduboy.setCursor(arduboy.width() - 3*8, 0); // font is 8px high
-  arduboy.print(x_position, DEC);
+  //arduboy.print(x_position, DEC);
+  arduboy.print(y_velocity, DEC);
+  
   arduboy.setCursor(arduboy.width() - 3*8, arduboy.height() - 8);
   arduboy.print(y_position, DEC);
 #endif
